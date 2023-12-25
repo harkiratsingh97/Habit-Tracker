bdc4142621bfbb6591fcfd2a7805a1f501d96625
harkiratsinghh <singh.harkirat97@gmail.com>
2023-12-25 17:00:53 +0530

    added environment.js

---
+++ config/environment.js
@@ -0,0 +1,5 @@
+const development = {
+	// db: "mongodb://127.0.0.1:27017/HabitTracker",
+	db:'mongodb+srv://harkirat:Q9hWPDq1sjVzJRqx@cluster0.s9jgarw.mongodb.net/?retryWrites=true&w=majority'
+};
+module.exports = development;
